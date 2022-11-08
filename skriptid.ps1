747d53d91141da4cd9da6d2753682a1b2ebc23d6
Karlis.Kaschan <karlis.kaschan@voco.ee>
2022-11-08 13:47:08 +0200

    first commit

---
+++ skriptid.ps1
@@ -0,0 +1,4 @@
+﻿$kasutajaparool = ConvertTo-SecureString "qwerty" -AsPlainText -force
+
+New-LocalUser "kasutaja1" -Password $kasutajaparool -fullname "esimene kasutaja" description "local account - kasutaja1"
+
