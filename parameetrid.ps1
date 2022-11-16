91c64a7a1f7079d853fcd2cb0bcaaea8b34d2a0e
Karlis.Kaschan <karlis.kaschan@voco.ee>
2022-11-16 11:26:54 +0200

    ss

---
+++ parameetrid.ps1
@@ -0,0 +1,7 @@
+﻿param(
+$kasutajanimi,
+$täisnimi,
+$kontokirjeldus
+)
+$kasutajaparool = ConvertTo-SecureString "Parool1!" -AsPlainText -force
+New-LocalUser $kasutajanimi -password $kasutajaparool -FullName $täisnimi -Description $kontokirjeldus
