b109ad9b47dcfae61d0b1fb049895559d1a7351e
Karlis.Kaschan <karlis.kaschan@voco.ee>
2022-11-16 11:30:53 +0200

    ss

---
+++ Kasutaja kustutamine.ps1
@@ -0,0 +1,5 @@
+﻿echo "kustutan kasutaja"
+$Eesnimi = read-host "sisesta kasutaja eesnimi"
+$Perenimi = read-host "sisesta kasutaja perenimi"
+$Taisnimi = $Eesnimi + " " + $Perenimi
+remove-localuser $Taisnimi -verbose
---
+++ kasutaja loomine.ps1
@@ -0,0 +1,13 @@
+﻿$KasutajaParool = ConvertTo-SecureString "Parool1" -AsPlainText -Force
+$Eesnimi = Read-Host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perenimi"
+$Kasutajanimi = $Eesnimi.ToLower() + " " + $Perenimi.ToLower()
+$Taisnimi = $Eesnimi + " " + $Perenimi
+$Kirjeldus = "Lokaalne konto- " + $Kasutajanimi
+Write-Output "Loodav kasutaja on $($Kasutajanimi)"
+$ErrorActionPreference = "SilentlyContinue"
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Taisnimi -Description $Kirjeldus
+if(!$?) {
+Write-Output "Tekkinud probleem kasutaja loomisega"
+}
+$ErrorActionPreference = "Stop"
